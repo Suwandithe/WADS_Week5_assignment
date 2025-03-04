3492480ae6b27b9f406cadbd7eb5a6a3536a3026
Suwandi The <144767691+Suwandithe@users.noreply.github.com>
2025-03-04 15:43:55 +0700

    firebase set up

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrXSb4HJiSBDq4suVdEIbayVQ_to2cyc8",
+  authDomain: "todo-app-a18a5.firebaseapp.com",
+  projectId: "todo-app-a18a5",
+  storageBucket: "todo-app-a18a5.firebasestorage.app",
+  messagingSenderId: "567002913122",
+  appId: "1:567002913122:web:455805f2b1d18369dc343b",
+  measurementId: "G-ZPX4F9LN69"
+};
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {db}
